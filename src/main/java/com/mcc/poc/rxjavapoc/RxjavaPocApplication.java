3cf5040eea9711b9ce5634b3a63c37c93c684c41
Miguel Ccantuta <mccantuta@yahoo.com>
2019-07-30 14:44:39 -0500

    Initial commit

---
+++ src/main/java/com/mcc/poc/rxjavapoc/RxjavaPocApplication.java
@@ -0,0 +1,13 @@
+package com.mcc.poc.rxjavapoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxjavaPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxjavaPocApplication.class, args);
+	}
+
+}
